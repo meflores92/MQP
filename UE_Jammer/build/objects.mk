8d62f54799352ca2ed006abfdeaeffa91132fb27
devon-poisson <70592811+devon-poisson@users.noreply.github.com>
2022-03-15 19:11:06 -0400

    Start uploading build folder

---
+++ UE_Jammer/build/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lgtk-3 -luhd -lpthread -lgomp -lm -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0
+
---
+++ UE_Jammer/build/sources.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ASM_SRCS := 
+C_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+C_DEPS := 
+EXECUTABLES := 
+OBJS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src/GUI \
+src/MATLAB_Code \
+src \
+src/OAI_CODE \
+src/timing \
+src/trx \
+
